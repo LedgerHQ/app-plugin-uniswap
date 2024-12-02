6b0e3f988449ead24f30da1777e95a72e7492633
Francois Beutin <francois.beutin@ledger.fr>
2024-12-02 13:27:15 +0100

    Update UI wording

--- src/handle_query_contract_ui.c
+++ src/handle_query_contract_ui.c
@@ -31,30 +31,30 @@ static bool format_address(char *msg, size_t msgLength, const uint8_t address[AD
 
 static bool asset_in(ethQueryContractUI_t *msg, const io_data_t *io_data) {
     if (io_data->asset_type == ETH) {
-        strlcpy(msg->title, "Sending", msg->titleLength);
+        strlcpy(msg->title, "Send", msg->titleLength);
         strlcpy(msg->msg, "Ethereum", msg->msgLength);
         return true;
     } else if (io_data->asset_type == KNOWN_TOKEN) {
-        strlcpy(msg->title, "Sending", msg->titleLength);
+        strlcpy(msg->title, "Send", msg->titleLength);
         strlcpy(msg->msg, io_data->u.token_info.ticker, msg->msgLength);
         return true;
     } else {
-        strlcpy(msg->title, "Sending token", msg->titleLength);
+        strlcpy(msg->title, "Send token", msg->titleLength);
         return format_address(msg->msg, msg->msgLength, io_data->u.address);
     }
 }
 
 static bool asset_out(ethQueryContractUI_t *msg, const io_data_t *io_data) {
     if (io_data->asset_type == ETH) {
-        strlcpy(msg->title, "Receiving", msg->titleLength);
+        strlcpy(msg->title, "Receive", msg->titleLength);
         strlcpy(msg->msg, "Ethereum", msg->msgLength);
         return true;
     } else if (io_data->asset_type == KNOWN_TOKEN) {
-        strlcpy(msg->title, "Receiving", msg->titleLength);
+        strlcpy(msg->title, "Receive", msg->titleLength);
         strlcpy(msg->msg, io_data->u.token_info.ticker, msg->msgLength);
         return true;
     } else {
-        strlcpy(msg->title, "Receiving token", msg->titleLength);
+        strlcpy(msg->title, "Receive token", msg->titleLength);
         return format_address(msg->msg, msg->msgLength, io_data->u.address);
     }
 }
@@ -74,24 +74,24 @@ static bool format_amount(char *msg, size_t msgLength, const io_data_t *io_data)
 
 static bool amount_in(ethQueryContractUI_t *msg, const context_t *context) {
     if (context->swap_type == EXACT_IN) {
-        strlcpy(msg->title, "Sending", msg->titleLength);
+        strlcpy(msg->title, "Send", msg->titleLength);
     } else {
-        strlcpy(msg->title, "Sending min.", msg->titleLength);
+        strlcpy(msg->title, "Send minimum", msg->titleLength);
     }
     return format_amount(msg->msg, msg->msgLength, &context->input);
 }
 
 static bool amount_out(ethQueryContractUI_t *msg, const context_t *context) {
     if (context->swap_type == EXACT_IN) {
-        strlcpy(msg->title, "Receiving min.", msg->titleLength);
+        strlcpy(msg->title, "Get minimum", msg->titleLength);
     } else {
-        strlcpy(msg->title, "Receiving", msg->titleLength);
+        strlcpy(msg->title, "Receive", msg->titleLength);
     }
     return format_amount(msg->msg, msg->msgLength, &context->output);
 }
 
 static bool pay_portion(ethQueryContractUI_t *msg, const context_t *context) {
-    strlcpy(msg->title, "Protocol fee", msg->titleLength);
+    strlcpy(msg->title, "Interface fee", msg->titleLength);
 
     // We can't use snprintf here because plugins are compiled without
     // Resort to doing manual computation
